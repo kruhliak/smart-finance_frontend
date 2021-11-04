b3c25205cc9aa418fcb276701f407b1672984c9d
Stanislav Kruhliak <stanislav.kruhliak@gmail.com>
2021-11-04 22:13:16 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { lazy, Suspense } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Loader from 'react-loader-spinner';
+
+const HomePage = lazy(() =>
+  import('views/HomePage' /* webpackChunkName: "home-page" */),
+);
+
+const ReportPage = lazy(() =>
+  import('views/ReportPage' /* webpackChunkName: "report-page" */),
+);
+const NotFoundPage = lazy(() =>
+  import('views/NotFoundPage' /* webpackChunkName: "not-found-page" */),
+);
+
+export default function App() {
+  return (
+    <div id="container">
+      <Suspense
+        fallback={<Loader type="Oval" color="#ff751d" height={50} width={50} />}
+      >
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+
+          <Route exact path="/report">
+            <ReportPage />
+          </Route>
+
+          <Route>
+            <NotFoundPage />
+          </Route>
+        </Switch>
+      </Suspense>
+    </div>
+  );
+}
---
+++ src/views/NotFoundPage.js
@@ -0,0 +1,7 @@
+export default function NotFoundView() {
+  return (
+    <div>
+      <h1>PAGE NOT FOUND</h1>
+    </div>
+  );
+}
