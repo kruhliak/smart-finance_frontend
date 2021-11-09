e10d8f11186d59461dda602eb1edcfa8c6d6d540
Nina Vasilieva <aninok92@gmail.com>
2021-11-09 17:31:43 +0200

    логин и регистрация

--- src/views/LoginPage/LoginPage.js
+++ src/views/LoginPage/LoginPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import {
   Wrapper,
@@ -9,8 +11,39 @@ import {
   StarErr,
   ButtonWrapper,
 } from './LoginPage.styled';
+import styled from '@emotion/styled';
 import Button from '../../components/Buttons/CustomButton';
+import { userLogin } from '../../redux/operations/auth-operation';
 
+const Link = styled(NavLink)`
+  text-decoration: none;
+  padding: 12px 0;
+  display: block;
+  width: 125px;
+  font-weight: bold;
+  font-size: 12px;
+  letter-spacing: 0.02em;
+  text-transform: uppercase;
+  outline: none;
+  border-radius: 16px;
+  text-align: center;
+  background-color: var(--white-text-color);
+  color: var(--primary-text-color);
+  border: 2px solid #f6f7fc;
+  box-shadow: 0px 0px 1px rgb(0 0 0 / 12%), 0px 1px 1px rgb(0 0 0 / 0%),
+    0px 2px 1px rgb(0 0 0 / 15%);
+
+  &.${props => props.activeClassName} {
+    color: red;
+  }
+
+  &:focus,
+  &:hover {
+    box-shadow: 2px 4px 4px 0px rgb(0 0 0 / 30%), -1px 0px 0px rgb(0 0 0 / 14%),
+      0px 2px 1px rgb(0 0 0 / 20%);
+    transition: box-shadow 200ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
+`;
 const LoginPage = () => {
   const {
     register,
@@ -19,7 +52,12 @@ const LoginPage = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = data => console.log(data);
+  const dispatch = useDispatch();
+
+  const onSubmit = data => {
+    console.log(data);
+    dispatch(userLogin(data));
+  };
 
   // console.log(watch('password')); // watch input value by passing the name of it
 
@@ -84,7 +122,9 @@ const LoginPage = () => {
         )}
         <ButtonWrapper>
           <Button text="Войти" type="submit" />
-          <Button text="Регистрация" backBtn type="submit" />
+          <Link to="/register" exact>
+            Регистрация
+          </Link>
         </ButtonWrapper>
       </Form>
     </Wrapper>
--- src/views/RegisterPage/RegisterPage.js
+++ src/views/RegisterPage/RegisterPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import {
   Wrapper,
   Form,
@@ -7,8 +9,41 @@ import {
   Label,
   ErrorMessage,
   StarErr,
+  ButtonWrapper,
 } from './RegisterPage.styled';
 import Button from '../../components/Buttons/CustomButton';
+import { userSignup } from '../../redux/operations/auth-operation';
+import styled from '@emotion/styled';
+
+const Link = styled(NavLink)`
+  text-decoration: none;
+  padding: 12px 0;
+  display: block;
+  width: 125px;
+  font-weight: bold;
+  font-size: 12px;
+  letter-spacing: 0.02em;
+  text-transform: uppercase;
+  outline: none;
+  border-radius: 16px;
+  text-align: center;
+  background-color: var(--white-text-color);
+  color: var(--primary-text-color);
+  border: 2px solid #f6f7fc;
+  box-shadow: 0px 0px 1px rgb(0 0 0 / 12%), 0px 1px 1px rgb(0 0 0 / 0%),
+    0px 2px 1px rgb(0 0 0 / 15%);
+
+  &.${props => props.activeClassName} {
+    color: red;
+  }
+
+  &:focus,
+  &:hover {
+    box-shadow: 2px 4px 4px 0px rgb(0 0 0 / 30%), -1px 0px 0px rgb(0 0 0 / 14%),
+      0px 2px 1px rgb(0 0 0 / 20%);
+    transition: box-shadow 200ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
+`;
 
 const RegisterPage = () => {
   const {
@@ -18,7 +53,12 @@ const RegisterPage = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = data => console.log(data);
+  const dispatch = useDispatch();
+
+  const onSubmit = data => {
+    console.log(data);
+    dispatch(userSignup(data));
+  };
 
   // console.log(watch('password')); // watch input value by passing the name of it
 
@@ -81,7 +121,12 @@ const RegisterPage = () => {
             <span>Not Error</span>
           </ErrorMessage>
         )}
-        <Button className="m-0-auto" text="Войти" type="submit" />
+        <ButtonWrapper>
+          <Button text="Регистрация" type="submit" />
+          <Link to="/login" exact>
+            Войти
+          </Link>
+        </ButtonWrapper>
       </Form>
     </Wrapper>
   );
