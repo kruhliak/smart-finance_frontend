033708a4304b2f1c4710e09ccb7bf1290fcad54f
Nina Vasilieva <aninok92@gmail.com>
2021-11-11 12:57:18 +0200

    Выносит ErrorMessage и навигацию в отдельный компонет

--- src/views/LoginPage/LoginPage.js
+++ src/views/LoginPage/LoginPage.js
@@ -1,55 +1,26 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { NavLink } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import {
   Wrapper,
   Form,
+  Text,
   Input,
   Label,
-  ErrorMessage,
   StarErr,
   ButtonWrapper,
 } from './LoginPage.styled';
-import styled from '@emotion/styled';
 import Button from '../../components/Buttons/CustomButton';
+import ButtonNav from '../../components/FormElems/ButtonNav/ButtonNav';
+import ErrorMessage from '../../components/FormElems/ErrorMessage/ErrorMessage';
 import { userLogin } from '../../redux/operations/auth-operation';
 
-const Link = styled(NavLink)`
-  text-decoration: none;
-  padding: 12px 0;
-  display: block;
-  width: 125px;
-  font-weight: bold;
-  font-size: 12px;
-  letter-spacing: 0.02em;
-  text-transform: uppercase;
-  outline: none;
-  border-radius: 16px;
-  text-align: center;
-  background-color: var(--white-text-color);
-  color: var(--primary-text-color);
-  border: 2px solid #f6f7fc;
-  box-shadow: 0px 0px 1px rgb(0 0 0 / 12%), 0px 1px 1px rgb(0 0 0 / 0%),
-    0px 2px 1px rgb(0 0 0 / 15%);
-
-  &.${props => props.activeClassName} {
-    color: red;
-  }
-
-  &:focus,
-  &:hover {
-    box-shadow: 2px 4px 4px 0px rgb(0 0 0 / 30%), -1px 0px 0px rgb(0 0 0 / 14%),
-      0px 2px 1px rgb(0 0 0 / 20%);
-    transition: box-shadow 200ms cubic-bezier(0.4, 0, 0.2, 1);
-  }
-`;
 const LoginPage = () => {
   const {
     register,
     handleSubmit,
-    // watch,
     formState: { errors },
+    reset,
   } = useForm();
 
   const dispatch = useDispatch();
@@ -57,17 +28,16 @@ const LoginPage = () => {
   const onSubmit = data => {
     console.log(data);
     dispatch(userLogin(data));
+    reset();
   };
-
-  // console.log(watch('password')); // watch input value by passing the name of it
 
   return (
     <Wrapper>
-      <p>Вы можете авторизоваться с помощью Google Account:</p>
+      <Text>Вы можете авторизоваться с помощью Google Account:</Text>
       <Button googleBtn text="Google"></Button>
-      <p>
+      <Text>
         Или зайти с помощью e-mail и пароля, предварительно зарегистрировавшись:
-      </p>
+      </Text>
       <Form onSubmit={handleSubmit(onSubmit)}>
         <Label htmlFor="email">
           {errors.email && <StarErr>*</StarErr>}
@@ -76,7 +46,7 @@ const LoginPage = () => {
         <Input
           id="email"
           placeholder="your@email.com"
-          placeholderTextColo="#A6ABB9"
+          placeholderTextColor="#A6ABB9"
           {...register('email', {
             required: 'это обязательное поле',
             pattern: {
@@ -86,13 +56,7 @@ const LoginPage = () => {
             },
           })}
         />
-        {errors.email ? (
-          <ErrorMessage>{errors.email.message}</ErrorMessage>
-        ) : (
-          <ErrorMessage opacity="true">
-            <span>Not Error</span>
-          </ErrorMessage>
-        )}
+        <ErrorMessage errors={errors.email} />
         <Label htmlFor="password">
           {errors.password && <StarErr>*</StarErr>}
           Пароль:
@@ -100,7 +64,7 @@ const LoginPage = () => {
         <Input
           id="password"
           placeholder="••••••••"
-          placeholderTextColo="#ffffff"
+          placeholderTextColor="#ffffff"
           {...register('password', {
             required: 'это обязательное поле',
             minLength: {
@@ -113,18 +77,10 @@ const LoginPage = () => {
             },
           })}
         />
-        {errors.password ? (
-          <ErrorMessage>{errors.password.message}</ErrorMessage>
-        ) : (
-          <ErrorMessage opacity="true">
-            <span>Not Error</span>
-          </ErrorMessage>
-        )}
+        <ErrorMessage errors={errors.password} />
         <ButtonWrapper>
           <Button text="Войти" type="submit" />
-          <Link to="/register" exact>
-            Регистрация
-          </Link>
+          <ButtonNav path="/register" text="Регистрация" />
         </ButtonWrapper>
       </Form>
     </Wrapper>
