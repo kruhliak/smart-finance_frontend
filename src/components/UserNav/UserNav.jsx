1de6012997f207d2e98a10835be81ec369d37f4f
Stanislav Kruhliak <stanislav.kruhliak@gmail.com>
2021-11-16 14:18:57 +0100

    edit UserNav firs render

--- src/components/UserNav/UserNav.jsx
+++ src/components/UserNav/UserNav.jsx
@@ -9,7 +9,7 @@ const UserNav = () => {
   const dispatch = useDispatch();
 
   const userName = useSelector(state => state.auth.user.name);
-  const firstUserNameLetter = userName.slice(0,1)
+  const firstUserNameLetter = userName?.slice(0, 1);
   const isMatches = useMediaQuery('(min-width: 768px)');
 
   return (
@@ -17,16 +17,16 @@ const UserNav = () => {
       <UserAvatar>{firstUserNameLetter}</UserAvatar>
       <P>{userName}</P>
       <LogoutBtn onClick={() => dispatch(userLogout())}>
-      
-      {isMatches ? 
-        <P>Выйти</P> : 
-        <Icon
-          name="icon-logout"
-          size="16px"
-          viewBox="0 0 16 16"
-          color="#CBCCD0"
-        ></Icon>}
-        
+        {isMatches ? (
+          <P>Выйти</P>
+        ) : (
+          <Icon
+            name="icon-logout"
+            size="16px"
+            viewBox="0 0 16 16"
+            color="#CBCCD0"
+          ></Icon>
+        )}
       </LogoutBtn>
     </NavContainer>
   );
