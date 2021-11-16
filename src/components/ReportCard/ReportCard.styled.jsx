67f30cae01c477cb9ee777d498c96ddce71144c7
Andres Link <andres.link@mail.ru>
2021-11-16 22:21:49 +0200

    visual fix

--- src/components/ReportCard/ReportCard.styled.jsx
+++ src/components/ReportCard/ReportCard.styled.jsx
@@ -4,15 +4,31 @@ export const Wrapper = styled.div`
   text-align: center;
   width: 90px;
   cursor: pointer;
+`;
+
+export const Background = styled.div`
+  position: absolute;
+  z-index: -1;
+  width: 59px;
+  height: 46px;
+  margin-left: 16px;
+  margin-top: 30px;
+  border-radius: 20px;
+  background-color: #f5f6fb;
+  ${({ selectedBg }) =>
+    selectedBg &&
+    `
+background-color: #FFDAC0;
+  `};
   @media (max-width: 768px) {
-    &:before {
-      content: '';
-      width: 280px;
-      border-bottom: 1px solid #e0e5eb;
+    &:after {
       position: absolute;
-      left: 127px;
-      margin: 20px;
-      margin-top: 128px;
+      content: '';
+      width: 100px;
+      height: 1px;
+      left: -20px;
+      bottom: -50px;
+      background-color: #e0e5eb;
     }
   }
 `;
