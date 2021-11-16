613bc74e8fb2500a1d37d550c50c4393831fc16a
Stanislav Kruhliak <stanislav.kruhliak@gmail.com>
2021-11-17 00:21:49 +0100

    edit financeAPI

--- src/services/financeAPI.js
+++ src/services/financeAPI.js
@@ -22,23 +22,65 @@ export async function deleteOperation(id) {
   return data;
 }
 
-// фильтрация по расходам или доходам
-//    {
-//       operation: "income" или "expense",
-//   }
-export async function operationByType(operation) {
-  const { data } = await axios.get(`/transactions/${operation}`);
+// для компонента сводка прилетает что то в этом стиле:
+// [
+//   {
+//     month: 10,
+//     sum: 3550,
+//   },
+//   {
+//     month: 11,
+//     sum: -13252.78,
+//   },
+// ];
+//localhost:3001/api/transactions/2021
+
+export async function getSummaryByYear(year) {
+  const { data } = await axios.get(`/transactions/${year}`);
   return data;
 }
 
-// фильтрация по расходам или доходам за указанный год
-export async function operationTypeByYear(operationType, year) {
-  const { data } = await axios.get(`/${operationType}/${year}`);
+//запасной вариант. прилетает обьект за месяц, отфильтрованный по расход/доход и с суммой елементов:
+// [
+//   {
+//     operation: 'income',
+//     sum: 5310,
+//     list: [{ item1 }, { item1 }],
+//   },
+// ];
+// [
+//   {
+//     operation: 'expense',
+//     sum: 7530,
+//     list: [{ itemNr1 }, { itemNr2 }],
+//   },
+// ];
+//localhost:3001/api/transactions/2021/11
+
+export async function getAllOperationByMonth(year, month) {
+  const { data } = await axios.get(`/transactions/${year}/${month}`);
   return data;
 }
 
-// фильтрация по расходам или доходам за указанный месяц года
-export async function operationTypeByMonth(operationType, year, month) {
-  const { data } = await axios.get(`/${operationType}/${year}/${month}`);
+// БОЕВАЯ ЕДИНИЦА: прилетает обьект за месяц, отфильтрованный по категориям и с суммой елементов
+// [
+//   {
+//     "category": "Продукты",
+//     sum: 5310,
+//     list: [{ item1 }, { item1 }],
+//   },
+// ];
+// [
+//   {
+//     category: 'Развлечение',
+//     sum: 7530,
+//     list: [{ itemNr1 }, { itemNr2 }],
+//   },
+// ];
+//localhost:3001/api/transactions/expense/2021/11
+export async function getCategoriesByMonth(operationType, year, month) {
+  const { data } = await axios.get(
+    `/transactions/${operationType}/${year}/${month}`,
+  );
   return data;
 }
