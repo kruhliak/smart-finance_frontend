ab1b648491b5dd662bb8db8cb5c2bd9262942146
Nina Vasilieva <aninok92@gmail.com>
2021-11-17 17:29:30 +0200

    удаление

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,22 +2,14 @@ import { useMediaQuery } from 'hooks/useMediaQuery';
 import TableMobile from './TableMobile/TableMobile';
 import TableDesktop from './TableDesktop/TableDesktop';
 
-const Table = ({ onClickModal, operation, color }) => {
+const Table = ({ operation, color }) => {
   const isMatches = useMediaQuery('(min-width: 768px)');
   return (
     <>
       {isMatches ? (
-        <TableDesktop
-          onClickModal={onClickModal}
-          operation={operation}
-          color={color}
-        />
+        <TableDesktop operation={operation} color={color} />
       ) : (
-        <TableMobile
-          onClickModal={onClickModal}
-          operation={operation}
-          color={color}
-        />
+        <TableMobile operation={operation} color={color} />
       )}
     </>
   );
