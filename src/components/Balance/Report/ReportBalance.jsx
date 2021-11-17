4b98d15b399c255c58045d971a96108f58e4ace9
EvgeniyKravchuk <gekahhh1996@gmail.com>
2021-11-17 18:42:36 +0200

    Edit balance
    Поисправлял в балансе некоторые недочеты: добавил маршруты где необходимо, заменил баланс на инпуты где это было необходимо, поисправлял проблемы позиционирования.

--- src/components/Balance/Report/ReportBalance.jsx
+++ src/components/Balance/Report/ReportBalance.jsx
@@ -1,28 +1,51 @@
+import { NavLink } from 'react-router-dom';
+
 import {
   ReportBox,
   ReportTitle,
   ReportText,
-  ReportLink,
+  ReportInputBox,
+  ReportInput,
   ReportButton,
+  MonthSlider,
 } from './ReportBalance.styled';
 
 import { Icon } from '../../../hooks/Icon';
 
 export default function ReportBalance() {
+  const screenWidth = document.documentElement.scrollWidth;
+  const renderOnTablet = screenWidth > 767;
+  const renderOnDesktop = screenWidth > 1279;
+
   return (
     <ReportBox>
-      <ReportLink>
+      <NavLink className={'report-balance-link'} to="/">
         <Icon
           name={'icon-back-arrow'}
           size={'18px'}
           viewBox="0 0 18 12"
           color="#FF751D"
         />
-        Вернуться на главную
-      </ReportLink>
-      <ReportTitle>Баланс:</ReportTitle>
-      <ReportText>55 000.00 UAH</ReportText>
-      <ReportButton>ПОДТВЕРДИТЬ</ReportButton>
+        {renderOnTablet && 'Вернуться на главную'}
+      </NavLink>
+      {!renderOnTablet && <MonthSlider>This is slider</MonthSlider>}
+      {renderOnDesktop && (
+        <form className="report-balance-form">
+          <ReportTitle>Баланс:</ReportTitle>
+          <ReportInputBox>
+            <ReportInput placeholder="55 000 UAH" />
+          </ReportInputBox>
+          <ReportButton>ПОДТВЕРДИТЬ</ReportButton>
+        </form>
+      )}
+      {!renderOnDesktop && (
+        <>
+          <ReportTitle>Баланс:</ReportTitle>
+          <ReportInputBox>55 000.00 UAH</ReportInputBox>
+        </>
+      )}
+
+      {renderOnTablet && <MonthSlider>This is slider</MonthSlider>}
     </ReportBox>
   );
 }
