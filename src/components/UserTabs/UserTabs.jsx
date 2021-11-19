34c66545e3bf98a4e73ddd027399a79d96b66aab
TarasovDR <d.r.tarasov03@gmail.com>
2021-11-19 22:42:47 +0200

    basic tabs styles

--- src/components/UserTabs/UserTabs.jsx
+++ src/components/UserTabs/UserTabs.jsx
@@ -1,6 +1,9 @@
+import React from 'react';
+import styled from '@emotion/styled/macro';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import { useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import ExpenseIncomeForm from 'components/ExpenseIncomeForm/ExpenseIncomeForm';
 import Table from 'components/Table/Table';
 import {
@@ -13,6 +16,80 @@ import {
 } from 'components/ExpenseIncomeForm/incomeList';
 import 'react-tabs/style/react-tabs.css';
 import Summary from 'components/Summary/Summary';
+import { device } from 'components/options/device';
+
+//............
+const STabs = styled(Tabs)`
+  -webkit-tap-highlight-color: transparent;
+  `;
+const STabList = styled(TabList)`
+  margin: 0;
+`;
+STabList.tabsRole = 'TabList';
+
+const STab = styled(Tab)`
+  display: inline-block;
+  position: relative;
+  list-style: none;
+  padding: 11px 44.5px;
+  cursor: pointer; //-
+  font-size: 0.75rem; //+
+  font-weight: 700; //+
+  text-transform: uppercase; //+
+  line-height: 17px; //+
+  color: var(--black-text-color); //+
+  background: linear-gradient(
+    270deg,
+    rgba(250, 251, 253, 1) 0%,
+    rgba(239, 239, 239, 1) 100%
+  ); //+
+  border-top-left-radius: 19px; //+
+  border-top-right-radius: 19px; //+
+
+  &.--selected {
+      background: #FEFEFE; //+
+      color: var(--accent-color); //+
+    }
+
+  &.is-disabled {
+    color: #eee;
+    cursor: default;
+  }
+
+  &:focus {
+    box-shadow: 0 0 5px var(--accent-color); //-
+    border-color: var(--accent-color); //-
+
+    &:after {
+      content: '';
+      position: absolute;
+      height: 5px;
+      left: -4px;
+      right: -4px;
+      bottom: -5px;
+      background: teal;
+    }
+  }
+`;
+STab.tabsRole = 'Tab';
+
+const STabPanel = styled(TabPanel)`
+  display: none;
+  background: var(--white-text-color);
+  box-shadow: 0px 10px 60px rgba(170, 178, 197, 0.2);
+  border-radius: 0px 30px 30px 30px;
+  
+  @media ${device.tablet} {
+    padding: 28px 30px 48px 30px;
+  }
+  @media ${device.desktop} {
+    padding: 33px 20px 61px 20px;
+  }
+  &.--selected {
+    display: block;
+  }
+`;
+STabPanel.tabsRole = 'TabPanel';
 
 
 const UserTabs = ({ onClickModal }) => {
@@ -24,27 +101,29 @@ useEffect(() => {
   state.length > 0 && state.map(i => 
   i.operation === 'income' ? setIncome(i) : setExpense(i)
   )
-
 }, [state])
 
-  return (
-  <Tabs>
-    <TabList>
-      <Tab>Расход</Tab>
-      <Tab>Доход</Tab>
-    </TabList>
 
-    <TabPanel>
+
+  return (
+    <STabs selectedTabClassName='--selected'
+    selectedTabPanelClassName='--selected'>
+    <STabList>
+      <STab>Расход</STab>
+      <STab>Доход</STab>
+    </STabList>
+
+    <STabPanel>
       <ExpenseIncomeForm list={expenseList} placeholder={expensePlaceholder} operationType={'expense'}/>
         <Table onClickModal={onClickModal} operation={expense} color={ true}/>
       <Summary />
-    </TabPanel>
-    <TabPanel>
+    </STabPanel>
+    <STabPanel>
       <ExpenseIncomeForm list={incomeList} placeholder={incomePlaceholder}  operationType={'income'}/>
        <Table onClickModal={ onClickModal} operation={income} color={false}/>
       <Summary />
-    </TabPanel>
-  </Tabs>
+    </STabPanel>
+  </STabs>
 )
 };
 
